269be06c7234aa7562293c2757b5e38ceb44d113
SrdjanPolic <srdjan.polic@yahoo.com>
2017-06-18 17:25:12 +0200

    Javascript part finished
    Javascript part finished

--- TheWorld/wwwroot/js/site.js
+++ TheWorld/wwwroot/js/site.js
@@ -2,37 +2,42 @@
 (function () {
 
 
-    var ele = $("#username");
-    ele.text("bla  prezime");
+    //var ele = $("#username");
+    //ele.text("bla  prezime");
 
-    var main = $("#main");
+    //var main = $("#main");
 
-    main.on("mouseenter", function () {
-        main.css('background-color', '#888');
+    //main.on("mouseenter", function () {
+    //    main.css('background-color', '#888');
+    //    console.log(1);
+    //});
+
+    //main.on("mouseleave", function () {
+    //    main.css('background-color', '');
+    //});
+
+    //var menuitems = $("ul.menu li a");
+    //menuitems.on("click", function () {
+    //    var me = $(this);
+    //    alert(me.text());
+    //});
+
+    var $sideBarAndWrapper = $("#sidebar,#wrapper");
+
+    var sidebarToggle = $("#sidebarToggle");
+    sidebarToggle.on("click", function () {
+
         console.log(1);
+        $sideBarAndWrapper.toggleClass("hide-sidebar");
+        if ($sideBarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Show sidebar");
+        }
+        else
+        {
+            $(this).text("Hide sidebar");
+        }
+       
     });
-
-    //$(document).on("mouseenter", "#main", function () {
-    //    main.style = "background-color: #888;";
-    //    console.log(2);
-    //});
-
-    main.on("mouseleave", function () {
-        main.css('background-color', '');
-    });
-
-    var menuitems = $("ul.menu li a");
-    menuitems.on("click", function () {
-        var me = $(this);
-        alert(me.text());
-    });
-
-    //var $sideBarAndWrapper = $("#sidebar,#wrapper");
-
-    //$("sidebarToggle").on("click", function () {
-
-    //    $sideBarAndWrapper.toggleClass("hide-sidebar");
-    //});
 
 
 })();
